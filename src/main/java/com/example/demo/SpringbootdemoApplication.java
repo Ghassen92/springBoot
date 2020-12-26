919c94201ca4d10f0f0c15a5292e40eb5a16f69b
ghassen92 <guesmighassen@yahoo.com>
2020-12-26 14:44:17 +0100

    add user

--- src/main/java/com/example/demo/SpringbootdemoApplication.java
+++ src/main/java/com/example/demo/SpringbootdemoApplication.java
@@ -1,13 +1,24 @@
 package com.example.demo;
 
+import com.example.demo.dao.EmployeeRepository;
+import com.example.demo.model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootdemoApplication {
+public class SpringbootdemoApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootdemoApplication.class, args);
     }
 
+    @Autowired
+    EmployeeRepository employeeRepository;
+    @Override
+    public void run(String... args) throws Exception {
+        employeeRepository.save(new Employee(null,"ghassen"));
+
+    }
 }
