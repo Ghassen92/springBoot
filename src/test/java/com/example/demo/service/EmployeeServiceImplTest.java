4f0d2eac22c1d88e4a65f2fcb62ad126e9077e3a
ghassen92 <guesmighassen@yahoo.com>
2020-12-26 14:07:07 +0100

    refactoring

--- src/test/java/com/example/demo/service/EmployeeServiceImplTest.java
+++ src/test/java/com/example/demo/service/EmployeeServiceImplTest.java
@@ -2,18 +2,12 @@ package com.example.demo.service;
 
 import com.example.demo.dao.EmployeeRepository;
 import com.example.demo.model.Employee;
-import org.junit.Before;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
